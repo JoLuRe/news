406419e115e4715b428eb95daace3059ccd95856
JoLuRe <jordi.lucio@gmail.com>
2017-05-10 23:49:00 +0200

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -1,22 +1,49 @@
-$(function(){
-    //cargar comunidades
-    $.getJSON( "data/comunidades.json", function( jsonObject ) {
-        ponerComunidades( jsonObject );
-    });
 
-    //onclick en verinfo
-    $("#verinfo").click(function() {
-        var v=$("#comunidades").val();
-        if (v!=0) window.location = "/comunidad/"+$("#comunidades").val();
-        else alert('Selecciona una comunidad');
-    });
+var jsonfiles = ["data/news1.json", "data/news2.json"];
+var contador = 0;
+
+$(document).ready(function(){
+    $("#loadmore").on("click", loadNews);
+    $(window).scrollTop(0);
 });
 
-function ponerComunidades(json){
-     $.each( json, function( i, comunidad ) {
-         //dos formas de hacer lo mismo, la segunda es más adecuada.
-         //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
-         $('#comunidades').append($('<option>', { value: comunidad.slug, text : comunidad.comunidad }));
-         //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
-     }); 
+$(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() == $(document).height())
+    {
+        loadNews();
+    }
+});
+
+function loadNews()
+{
+     if(contador < jsonfiles.length)
+    {
+        $.getJSON(jsonfiles[contador], function(jsonObject){
+            var items = [];
+            var str;
+            $.each(jsonObject, function(i, gossip){
+                str = "";
+                str += "<div class='col-sm-10 col-md-7'><br>";
+                str += "<kbd class='fecha'>" + gossip.datetime + "</kbd>";
+                str += "<mark class='titulo'><span class='glyphicon glyphicon-circle-arrow-right'></span>" + gossip.title + "</mark>";
+                str += "<p class='descripcion1'>" + gossip.description1 + "</p>";
+                str += "<p class='descripcion2'>" + gossip.description2 + "</p>";
+                str += "</div>";
+                str += "<div class='col-sm-2 col-md-2'><br>";
+                str += "<img class='img-rounded img-responsive' src='img/" + gossip.imgmid + "' />";
+                str += "</div>";
+                items.push(str);
+            });
+
+            $("<div/>", {
+//                  "class": "row",
+                    html: items.join("")
+            }).appendTo("#more_news");
+        });
+        contador++;
+    }
+    if(contador >= jsonfiles.length)
+    {
+          alert("Nada más por hoy");
+    }
 }
